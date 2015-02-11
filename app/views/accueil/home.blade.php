b6426073e12a7cf6085a2f37fb34a16b714b008a
ayasum <ayasum@musaya.net>
2015-02-11 22:37:36 +0100

    added stylish picture on home

--- app/views/accueil/home.blade.php
+++ app/views/accueil/home.blade.php
@@ -2,8 +2,12 @@
 @section('content')
 <div class="container content-container">
 	<div class="row">
-		<div class="col-sm-8">
-			<h1 class="animate-page-title">Association ASFL21</h1>
+		<div class="col-sm-12">
+			<h1 class="animate-page-title">Association des Sages Femmes Libérales de Côte d'Or</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-6">
 			<p>En s'unissant en association, les sages femmes libérales de Côte d'Or souhaitent offrir une information et un accompagnement personnalisé à chaque femme, couple et enfant.</p> 
 			<p>Bienvenue sur notre site.</p>
 			<p>
@@ -15,6 +19,9 @@
 					<li><a href="{{ URL::route('message.index') }}"><i class="fa fa-long-arrow-right"></i> Espace PRO</a></li>
 				</ul>
 			</p>
+		</div>
+		<div class="col-sm-6">
+			<img class="img-responsive img-rounded" id="test-image" src="{{ asset('assets/img/feet-619534_1280.jpg') }}">
 		</div>
 	</div>
 </div>
