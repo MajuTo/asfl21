95bded06b4123709633127010aab6c919847f03a
musaya <ayasum@musaya.net>
2016-02-24 17:10:34 +0100

    did you know, there was content here?

--- app/views/accueil/home.blade.php
+++ app/views/accueil/home.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.index')
 @section('content')
 <section id="accueil">
-	<div class="row">
-		<div class="col-sm-12">
-			<h1>Association des Sages Femmes Libérales de Côte d'Or</h1>
-		</div>
-	</div>
+	
 
 	<!-- CAROUSEL -->
 	<div id="carousel-home" class="carousel slide" data-ride="carousel" data-interval="8000">
@@ -41,6 +37,12 @@
 	</div>
 	<!-- END CAROUSEL -->
 
+	<div class="row">
+		<div class="col-sm-12">
+			<h1 id="accueil-h1">Association des Sages Femmes Libérales de Côte d'Or</h1>
+		</div>
+	</div>
+
 	<!-- ACCUEIL TEXT -->
 	<div class="row">
 		<div class="text-justify" id="accueil-text">
@@ -48,11 +50,11 @@
 			<p><i class="fa fa-comments"></i> Parce qu'<strong>être accompagnée</strong> et guidée dans sa féminité et sa maternité est essentiel.</p>
 			<p><i class="fa fa-question-circle"></i> Parce qu'on ne sait pas toujours ce qui <strong>s'offre à nous</strong>.</p>
 			<p><i class="fa fa-group"></i> Les sages-femmes libérales de la Côte-d'Or ont souhaité s'unir en une association</p>
-			<p>pour proposer à <strong>chaque femme, couple et enfant</strong></p>
-			<p>une information claire et un accompagnement adapté à chacun.</p>
+			<p>pour proposer à <strong>chaque femme, couple et enfant</strong> une information claire et un accompagnement adapté à chacun.</p>
 		</div>
 	</div>
-<!-- 	<div class="row">
+<!-- 	<
+	div class="row">
 		<div class="col-sm-6">
 			<ul id="home-li pull-right">
 				<li><a href="{{ URL::route('notremetier') }}"><i class="fa fa-long-arrow-right"></i> Que me propose ma sage femme Libérale ?</a></li>
@@ -65,7 +67,8 @@
 		<div class="col-sm-6">
 			<img class="img-responsive img-rounded" id="feet" src="{{ asset('assets/img/pregnant-collection/feet-619534_1280.jpg') }}" alt="pieds de bébé">
 		</div>
-	</div> -->
+	</div>
+-->
 </section>
 @stop
 
