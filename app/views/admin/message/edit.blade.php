73821a199203a58d6fefd00bbb53a4e834ff6617
MajuTo <majuto@free.fr>
2016-03-23 16:00:22 +0100

    CKEditor dans l'édition des messages en partie admin

--- app/views/admin/message/edit.blade.php
+++ app/views/admin/message/edit.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.admin')
 @section('content')
+<script src="//cdn.ckeditor.com/4.5.7/basic/ckeditor.js"></script>
     {{ BootForm::openHorizontal(3, 9)->put()->action(URL::route('admin.message.update', $message->id)) }}
         {{ Form::token() }}
         {{ BootForm::bind($message) }}
         {{ BootForm::text('Titre', 'title')->placeHolder('Titre...')->required() }}
-        {{ BootForm::text('Message', 'content')->placeHolder('Message...')->required() }}
+        {{ BootForm::textarea('Message', 'content')->placeHolder('Message...')->required() }}
         {{ BootForm::select('Catégorie', 'category_id')->options($categories) }}
         {{ BootForm::submit('Enregistrer', 'pull-right btn-pink') }}
     {{ BootForm::close() }}
@@ -12,6 +13,7 @@
 @section('script')
     <script>
         $(document).ready(function(){
+            CKEDITOR.replace( 'content' );
             $('#nav-membre').addClass('active');
             $('#nav-admin').addClass('active');
             $('#nav-admin-messages').addClass('active');
