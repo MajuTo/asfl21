06b2023c69e4a0f3be310044bdb8187c29a8162a
Thomas RAY <majuto@free.fr>
2017-03-03 10:07:12 +0100

    Ajout du package Geocoder pour la localisation, pays par défaut à France

--- app/controllers/AddressController.php
+++ app/controllers/AddressController.php
@@ -63,14 +63,18 @@ class AddressController extends \BaseController {
 			$address->user_id = Auth::id();
 		}
 
-		$location = $this->geocode($address);
-		if ($location != null) {
-			$address->lat = $location['lat'];
-			$address->lng = $location['lng'];
-		} else {
-			Alert::add('alert-warning', "Cette adresse n'existe pas dans Google Maps.");
-		}
+		$address->country = 'FRANCE';
+
+		// $location = $this->geocode($address);
+		// if ($location != null) {
+		// 	$address->lat = $location['lat'];
+		// 	$address->lng = $location['lng'];
+		// } else {
+		// 	Alert::add('alert-warning', "Cette adresse n'existe pas dans Google Maps.");
+		// }
 		
+		$address = $this->geocode($address);
+
 		//Sauvegarde
 		$address->save();
 
@@ -153,17 +157,21 @@ class AddressController extends \BaseController {
 			$address->user_id = Auth::id();
 		}
 
-		$location = $this->geocode($address);
-		if ($location == null) {
-			Alert::add('alert-warning', "Cette adresse n'existe pas dans Google Maps.");
-			$address->lat = null;
-			$address->lng = null;
-			$address->save();
-		} else {
-			$address->lat = $location['lat'];
-			$address->lng = $location['lng'];
-			$address->save();
-		}
+		$address = $this->geocode($address);
+		$address->save();
+
+		// if ($location == null) {
+		// 	Alert::add('alert-warning', "Cette adresse n'existe pas dans Google Maps.");
+		// 	$address->lat = null;
+		// 	$address->lng = null;
+		// 	$address->save();
+		// } else {
+		// 	$address->lat = $location['lat'];
+		// 	$address->lng = $location['lng'];
+		// 	$address->save();
+		// }
+		// var_dump($address);
+		// die();
 
 		Alert::add("alert-success", "L'adresse a bien été enregistrée");
 
@@ -195,38 +203,55 @@ class AddressController extends \BaseController {
 
 	public function geocode($address){
 		// a changer en prod... clé de dev.
-		$googleApiKey = 'AIzaSyAqJKkv1M-fhJ97dtL0CUV16QelltipJmE';
-		$parameters   = str_replace(' ', '+', $address->address . ' ' . $address->zipCode . ' ' . $address->city . "&key=" . $googleApiKey);
-		$googleMapUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $parameters;
+		// $googleApiKey = 'AIzaSyAqJKkv1M-fhJ97dtL0CUV16QelltipJmE';
+		// $parameters   = str_replace(' ', '+', $address->address . ' ' . $address->zipCode . ' ' . $address->city . '+France' . "&key=" . $googleApiKey);
+		// $googleMapUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $parameters;
 
-		$ch = curl_init($googleMapUrl);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		// var_dump($googleMapUrl);
 
-
-	    try{
-			$exec = curl_exec($ch);
-			if($exec == false){
-				throw new Exception(curl_error($ch), curl_errno($ch));
-			}
-	    }catch(Exception $e){
-	    	trigger_error(sprintf(
-		        'Curl failed with error #%d: %s',
-		        $e->getCode(), $e->getMessage()),
-		        E_USER_ERROR);
-	    }
+		// $ch = curl_init($googleMapUrl);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 
+	 //    try{
+		// 	$exec = curl_exec($ch);
+		// 	if($exec == false){
+		// 		throw new Exception(curl_error($ch), curl_errno($ch));
+		// 	}
+	 //    }catch(Exception $e){
+	 //    	trigger_error(sprintf(
+		//         'Curl failed with error #%d: %s',
+		//         $e->getCode(), $e->getMessage()),
+		//         E_USER_ERROR);
+	 //    }
 
-	    $geolocation = json_decode(curl_exec($ch), true);
+		// var_dump($googleMapUrl);
 
-	    if ($geolocation['status'] == 'OK') {
-	    	$lat = $geolocation['results'][0]['geometry']['location']['lat'];
-			$lng = $geolocation['results'][0]['geometry']['location']['lng'];
-		    return ['lat' => $lat, 'lng' => $lng];
-	    }
 
-	    return null;
+	 //    $geolocation = json_decode(curl_exec($ch), true);
+	 //    var_dump($ch, $geolocation);
+
+	 //    if ($geolocation['status'] == 'OK') {
+	 //    	$lat = $geolocation['results'][0]['geometry']['location']['lat'];
+		// 	$lng = $geolocation['results'][0]['geometry']['location']['lng'];
+		//     return ['lat' => $lat, 'lng' => $lng];
+	 //    }
+
+		try {
+			$geocode = Geocoder::geocode($address->address . ', ' . $address->zipCode . ', ' . $address->city . ', ' . $address->country);
+			// The GoogleMapsProvider will return a result
+			$address->lat = $geocode->getLatitude();
+			$address->lng = $geocode->getLongitude();
+		} catch (Exception $e) {
+			Alert::add('alert-warning', "Cette adresse n'existe pas dans Google Maps.");
+			$address->lat = null;
+			$address->lng = null;
+		}
+
+		return $address;
+
+	    // return null;
 
 	}
 
