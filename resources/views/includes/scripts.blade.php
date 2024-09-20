bc7f8b38fae694300047e66ddd6a1f7591d34a90
majuto <majuto@free.fr>
2024-09-20 18:33:35 +0200

    Upgrade CKEditor 4.13 -> 4.25-lts

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,5 +1,5 @@
 {{ Html::script('assets/js/jquery-3.4.1.min.js') }}
 {{ Html::script('assets/bootstrap/js/bootstrap.min.js') }}
-{{ Html::script('https://cdn.ckeditor.com/4.13.1/basic/ckeditor.js') }}
+{{ Html::script('https://cdn.ckeditor.com/4.25.0-lts/basic/ckeditor.js') }}
 {{--{{ Html::script('assets/leaflet/leaflet.js') }}--}}
 {{ Html::script('assets/js/app.js') }}
